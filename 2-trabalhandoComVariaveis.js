271aa6358948227bc8a56443b02c7efbb14f6b5f
Luciana Moraes <lucianamoraestx@gmail.com>
2021-11-16 16:06:42 -0300

    Conversão de tipos - Alura

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+idade = 29;
+Idade = 30;
+
+console.log(idade);
---
+++ 4-ConversaoDeTipos.js
@@ -0,0 +1,13 @@
+console.log("conversão de Tipos")
+
+console.log("ano" + 2020); 
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2")); //A soma tem que converter com parseInt, pois é especial, também é usada para concatenar 
+//parseFloat é para números decimais
+console.log("10" / "2"); //uma divisão só faz sentido com números, por isso o JS já faz automaticamente
+console.log("Ricardo" / "2"); //Vai retornar NaN = Not a Number 
+
+console.log(7 / 2); //ponto flutuante, números decimais
+console.log(6.5); //sempre usar . (ponto flutuante)
+console.log(6,5); //A vírgula é um cactere especial, usado para dar espaço
+
