79a73f5996b785188ae98c256cec846a42a8baa0
Luciana Moraes <lucianamoraestx@gmail.com>
2021-11-17 13:06:12 -0300

    Módulo 04 - Condicionais e fluxo de execução - Aula 02

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,22 +6,37 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 17;
-const estaAcompanhada = true; //booleano, que representa valores verdadeiros ou falsos
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhada) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1, 1);
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1);
+// } else if (estaAcompanhada) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1);
 
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem");
+    listaDeDestinos.splice(2, 1);
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
 
-console.log(listaDeDestinos);
+console.log("Embarque: \n\n");
+if(idadeComprador > 18 && temPassagemComprada){
+    console.log("Boa viagem");
+}else{
+    console.log("Você não pode embarcar");
+}
+
+//console.log(listaDeDestinos);
 
 
