9f5f58b59dead0cacfa02a5582f4c90671aa8dd4
Luciana Moraes <lucianamoraestx@gmail.com>
2021-11-17 08:20:46 -0300

    Listas - removendo elementos

--- 6-listas.js
+++ 6-listas.js
@@ -13,3 +13,12 @@ listaDeDestinos.push(`Curitiba`); //adicionando um item na lista
 console.log("Destinos possíveis");
 //console.log(salvador, saoPaulo, rioDeJaneiro);
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1); //remover um item da lista 
+//(1 posição do item na lista, 1 quantidade de itens que será removido)
+//a contagem começa no "0", o item que será removido é São Paulo
+console.log(listaDeDestinos);
+
+console.log(listaDeDestinos[1]); //imprimir somente um item da lista
+//[1 posição do item na lista]
+ 
