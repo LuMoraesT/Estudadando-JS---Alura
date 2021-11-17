02480c4182481422f0c50b910bb52d1feb2f2c28
Luciana Moraes <lucianamoraestx@gmail.com>
2021-11-17 11:30:56 -0300

    Ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,21 +6,22 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 17;
+const estaAcompanhada = true; //booleano, que representa valores verdadeiros ou falsos
+
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18) {
+if (idadeComprador >= 18) {
     console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1); //remover um item 
-}else{
+    listaDeDestinos.splice(1, 1);
+} else if (estaAcompanhada) {
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1, 1);
+
+} else {
     console.log("Não é maior de idade e não posso vender");
-} 
+}
 
 console.log(listaDeDestinos);
 
-// console.log(idadeComprador >18);
-// console.log(idadeComprador >18);
-// console.log(idadeComprador <=18);
-// console.log(idadeComprador >18);
-// console.log(idadeComprador ==18); **quando for igual, usar duas vezes ==, pois se for só uma, estará atribuindo um valor.
- 
+
